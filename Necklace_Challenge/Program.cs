bae6da9644f9505f45af7804effb017d50cb321e
Duncan Phelps <phelps.duncan@gmail.com>
2020-05-13 15:53:10 -0500

    first commit

---
+++ Necklace_Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Necklace_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
